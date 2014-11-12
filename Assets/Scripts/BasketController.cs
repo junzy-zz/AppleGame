c04753e685fce443f547a82170024c5e0a7e07b9
junzy <junzy1994@gmail.com>
2014-11-12 16:21:01 +0530

    2nd Commit
    V1

---
+++ Assets/Scripts/BasketController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasketController : MonoBehaviour {
+
+	// Use this for initialization
+	public int Score;
+	public float Speed;
+	//public GUIText scoreText;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown("left"))
+		{
+			transform.position = Vector2.Lerp(transform.position, (new Vector2(-11.0f,this.transform.position.y)), Speed);
+			//this.transform.position=  new Vector2(-11.0f,this.transform.position.y);
+		}
+		if(Input.GetKeyDown("right"))
+			//this.transform.position= new Vector2(9.0f,this.transform.position.y);
+			transform.position = Vector2.Lerp(transform.position, (new Vector2(9.0f,this.transform.position.y)), Speed);
+	}
+
+	void OnCollisionStay2D(Collision2D other)
+	{
+		if(Input.GetKeyDown("space"))
+		{
+			Destroy(other.gameObject);
+			Score=Score + 2;
+	//		scoreText.text = "Score: " + Score;
+		}
+		//Score--;
+
+	}
+
+
+}
